3c501898369a65306667014b8c5f3ebad64daea6
caoz <caoz@ctyi.com.cn>
2019-05-16 18:42:56 +0800

    up

--- web/application/admin/controller/article.php
+++ web/application/admin/controller/article.php
@@ -10,15 +10,94 @@ class Article
 {
     public function index()
     {
+    	$Article = model('Article');
+    	$list = $Article->articleList();
+
 		$view = new View();
-		//$view->assign('list', $list);
+		$view->assign('list', $list);
 		return $view->fetch('admin/article-list');
 	}
 	
-	public function test()
+	public function addArticle()
 	{
-		//echo 'test';
-		phpinfo();
+		$Article = model('Article');
+		$category = $Article->categoryList();
+		$category_tree = $this->get_tree($category, 0);
+
+    	$view = new View();
+		$view->assign('category', $category_tree);
+		return $view->fetch('admin/article-add');
 		
+	}
+
+	public function add()
+	{
+		$category_id 	= !empty($_REQUEST['category_id']) ? intval($_REQUEST['category_id']) : 0;
+		$title 			= !empty($_REQUEST['title']) ? trim($_REQUEST['title']) : '';
+		$content 		= !empty($_REQUEST['editorValue']) ? $_REQUEST['editorValue'] : '';
+
+		$Article = model('Article');
+		$result = $Article->addArticle($content, $category_id, $title);
+
+		if($result)
+		{
+			exit("<script>window.location.href='index';</script>");
+		}
+		else
+		{
+			exit("<script>alert('添加失败');</script>");
+		}
+	}
+
+	public function editArticle()
+	{
+		$id 			= !empty($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
+
+		$Article = model('Article');
+    	$list = $Article->articleList($id);
+
+    	$category = $Article->categoryList();
+		$category_tree = $this->get_tree($category, 0);
+
+    	$view = new View();
+		$view->assign('category', $category_tree);
+		$view->assign('article', $list[0]);
+		return $view->fetch('admin/article-edit');		
+	}
+
+	public function edit()
+	{
+		$category_id 	= !empty($_REQUEST['category_id']) ? intval($_REQUEST['category_id']) : 0;
+		$title 			= !empty($_REQUEST['title']) ? trim($_REQUEST['title']) : '';
+		$content 		= !empty($_REQUEST['editorValue']) ? $_REQUEST['editorValue'] : '';
+
+		$id 	= !empty($_REQUEST['article_id']) ? intval($_REQUEST['article_id']) : 0;
+		$Article = model('Article');
+		$result = $Article->editArticle($content, $category_id, $title, $id);
+
+		if($result)
+		{
+			exit("<script>window.location.href='index';</script>");
+		}
+		else
+		{
+			exit("<script>alert('编辑失败');</script>");
+		}
+	}
+
+
+	public function get_tree($data, $pid)
+	{
+	    $tree = array();
+	    foreach($data as $k => $v)
+	    {
+	      if($v['parent_id'] == $pid)
+	      {        
+	       //父亲找到儿子
+	       $v['children'] = $this->get_tree($data, $v['id']);
+	       $tree[] = $v;
+	      }
+	    }
+	    return $tree;
 	}
 }
