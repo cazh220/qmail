d9127d2e391b48d8c2866cfc4e59cd329fa6aeee
caoz <caoz@ctyi.com.cn>
2019-05-31 21:19:02 +0800

    更新

--- web/application/admin/controller/Customer.php
+++ web/application/admin/controller/Customer.php
@@ -25,6 +25,50 @@ class Customer extends Controller
 		return $view->fetch('admin/customer_add');
 	}
 
+	public function add()
+	{
+		$name = !empty($_POST['username']) ? trim($_POST['username']) : '';
+		$weixin = !empty($_POST['weixin']) ? trim($_POST['weixin']) : '';
+		$qq = !empty($_POST['qq']) ? trim($_POST['qq']) : '';
+		$mobile = !empty($_POST['mobile']) ? trim($_POST['mobile']) : '';
+		$email = !empty($_POST['email']) ? trim($_POST['email']) : '';
+		$users_num = !empty($_POST['users_num']) ? intval($_POST['users_num']) : 0;
+		$end_time = !empty($_POST['end_time']) ? trim($_POST['end_time']) : '';
+		$uses_info = !empty($_POST['uses_info']) ? trim($_POST['uses_info']) : '';
+		$track_info = !empty($_POST['track_info']) ? trim($_POST['track_info']) : '';
+		$note = !empty($_POST['note']) ? trim($_POST['note']) : '';
+
+		$data = array(
+			'name'		=> $name,
+			'weixin'	=> $weixin,
+			'qq'		=> $qq,
+			'mobile'	=> $mobile,
+			'email'		=> $email,
+			'users_num'	=> $users_num,
+			'end_time'	=> $end_time,
+			'uses_info'	=> $uses_info,
+			'track_info'=> $track_info,
+			'note'		=> $note
+			);
+		//判断用户是否存在
+		$Customer = model('Customer');
+		if($Customer->getCustomerByName($name) || $Customer->getCustomerByMobile($mobile))
+		{
+			exit(json_encode(array('code'=>0,'msg'=>'用户名或手机号已存在')));
+		}
+
+		$result = $Customer->addCustomer($data);
+
+		if($result)
+		{
+			exit(json_encode(array('code'=>1,'msg'=>'success')));
+		}
+		else
+		{
+			exit(json_encode(array('code'=>0,'msg'=>'添加失败')));
+		}
+	}
+
 	
 	
 }
---
+++ web/application/admin/model/Customer.php
@@ -0,0 +1,100 @@
+<?php
+namespace app\admin\model;
+
+use think\Model;
+use think\Db;
+use think\Paginator;
+
+class Customer extends Model
+{
+
+	public function addCustomer($data = array())
+	{
+		return  Db::execute('insert into customer(name,weixin,mobile,qq,email,end_time,users_num,uses_info,track_info,note,create_time)values(?,?,?,?,?,?,?,?,?,?,?)',[$data['name'],$data['weixin'],$data['mobile'],$data['qq'], $data['email'], $data['end_time'], $data['users_num'], $data['uses_info'], $data['track_info'], $data['note'], date("Y-m-d H:i:s")]);
+	}
+
+	public function getCustomerByName($name='')
+	{
+		$res = Db::query("select *  from customer where is_delete = 0 and name = ?", [$name]);
+		return $res ? $res : array();
+	}
+
+	public function getCustomerByMobile($mobile = '')
+	{
+		$res = Db::query("select *  from customer where is_delete = 0 and mobile = ?", [$mobile]);
+		return $res ? $res : array();
+	}
+	/*
+	public function categoryList()
+	{
+		$res = Db::query("select *  from article_category where is_delete = ?", [0]);
+		return $res ? $res : array();
+	}
+
+	public function addArticle($content='', $category_id=0, $title='', $brief='', $thumb_pic = '')
+	{
+		return Db::execute('insert into article (title,category_id, content, status, brief, update_time, thumb_pic)values(?,?,?,?,?,?,?)',[$title,$category_id,$content,1, $brief, date("Y-m-d H:i:s"), $thumb_pic]);
+	}
+
+	public function editArticle($content='', $category_id=0, $title='', $brief='', $thumb_pic='', $id = 0)
+	{
+		return Db::execute('update article set title = ?,category_id = ?, content = ?, update_time = ?, brief = ?, thumb_pic = ? where id = ?',[$title,$category_id,$content, date("Y-m-d H:i:s"), $brief, $thumb_pic, $id]);
+	}
+
+	public function articleList($id=0)
+	{
+		if($id)
+		{
+			$res = Db::query("select a.id,a.title, a.brief, a.category_id,a.content,a.update_time,b.category_name,a.thumb_pic from article a left join article_category b on a.category_id = b.id where a.id = ?", [$id]);
+		}
+		else
+		{
+			$res = Db::query("select a.id,a.brief,a.title,a.category_id,a.content,a.update_time,b.category_name,a.thumb_pic from article a left join article_category b on a.category_id = b.id");
+		}
+
+		return $res;
+	}
+	
+	public function deleteArticle($id)
+	{
+		return Db::execute('delete from article where id = ?',[$id]);
+	}
+
+
+	public function addCategory($category_name='', $parent_id='')
+	{
+		return Db::execute('insert into article_category (category_name,parent_id)values(?,?)',[$category_name,$parent_id]);
+	}
+	
+	public function topCategory()
+	{
+		$res = Db::query("select *  from article_category where parent_id = ?", [0]);
+		return $res ? $res : array();
+	}
+	
+	
+	
+	public function category_name($category_id)
+	{
+		$res = Db::query("select *  from article_category where id = ?", [$category_id]);
+		return $res[0]['category_name'] ? $res[0]['category_name'] : array();
+	}
+	
+	public function delCategory($category_id)
+	{
+		return Db::execute('delete from article_category where id = ? or parent_id = ?',[$category_id,$category_id]);
+	}
+	
+	public function editCategory($id, $name, $parent_id=0)
+	{
+		return Db::execute('update article_category SET category_name = ?, parent_id = ?  where id = ? ',[$name,$parent_id,$id]);
+	}
+	
+	public function getCategory($id)
+	{
+		$res = Db::query("select *  from article_category where id = ?", [$id]);
+		return $res[0] ? $res[0] : array();
+	}
+	*/
+
+}
