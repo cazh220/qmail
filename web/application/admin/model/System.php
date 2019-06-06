50010d8137c532d069a31242c7eabaf2d07e657e
caoz <caoz@ctyi.com.cn>
2019-06-06 17:13:49 +0800

    up

--- web/application/admin/model/System.php
+++ web/application/admin/model/System.php
@@ -10,7 +10,7 @@ class System extends Model
 	public function saveSystem($data)
 	{
 		//print_r($data);die;
-		return Db::execute('update system SET company = ?, tel = ?, mobile = ?, email = ?, address = ?, qq = ?, price = ?  where id = 1 ',[$data['name'],$data['tel'],$data['mobile'], $data['email'], $data['address'], $data['qq'], $data['price']]);
+		return Db::execute('replace system SET company = ?, tel = ?, mobile = ?, email = ?, address = ?, qq = ?, price = ?  where id = 1 ',[$data['name'],$data['tel'],$data['mobile'], $data['email'], $data['address'], $data['qq'], $data['price']]);
 	}
 	
 	public function getSystem()
