1196499791f383fe1fc720dc28509cf581e40e88
caoz <caoz@ctyi.com.cn>
2019-06-06 17:23:26 +0800

    up

--- web/application/admin/model/System.php
+++ web/application/admin/model/System.php
@@ -9,20 +9,21 @@ class System extends Model
 {
 	public function saveSystem($data)
 	{
-		if($this->getSystem())
+		$info = $this->getSystem();
+		if(!empty($info[0]['id']))
 		{
-			return Db::execute('update system SET company = ?, tel = ?, mobile = ?, email = ?, address = ?, qq = ?, price = ?  where id = 1 ',[$data['name'],$data['tel'],$data['mobile'], $data['email'], $data['address'], $data['qq'], $data['price']]);
+			return Db::execute('update system SET company = ?, tel = ?, mobile = ?, email = ?, address = ?, qq = ?, price = ?  where id = ?',[$data['name'],$data['tel'],$data['mobile'], $data['email'], $data['address'], $data['qq'], $data['price'], $info[0]['id']]);
 		}
 		else
 		{
-			return Db::execute('insert into SET company = ?, tel = ?, mobile = ?, email = ?, address = ?, qq = ?, price = ?',[$data['name'],$data['tel'],$data['mobile'], $data['email'], $data['address'], $data['qq'], $data['price']]);
+			return Db::execute('insert into system SET company = ?, tel = ?, mobile = ?, email = ?, address = ?, qq = ?, price = ?',[$data['name'],$data['tel'],$data['mobile'], $data['email'], $data['address'], $data['qq'], $data['price']]);
 		}
 		
 	}
 	
 	public function getSystem()
 	{
-		$res = Db::query("select * from system where id = 1");
+		$res = Db::query("select * from system limit 1");
 		return $res ? $res : array();
 	}
 }
