9101e081ae93996b43c52be933d058fd777d2aa3
caoz <caoz@ctyi.com.cn>
2019-06-06 17:28:23 +0800

    up

--- web/application/index/Base.php
+++ web/application/index/Base.php
@@ -14,15 +14,15 @@ class Base
 	public function __construct()
 	{
 		$data = Db::query("select * from system limit 1");
-		Session::set('system_info',$data[0]);
+		Session::set('system_info',!empty($data[0])?$data[0]:array());
 		
 		//顶部左侧图片
 		$pic = Db::query("select * from pictures where type = 97 limit 1");
-		Session::set('top_left_picture',$pic[0]);
+		Session::set('top_left_picture',!empty($pic[0])?$pic[0]:array());
 
 		//二维码
 		$qrc = Db::query("select * from pictures where type = 96 limit 1");
-		Session::set('qrcode',$qrc[0]);
+		Session::set('qrcode',!empty($qrc[0])?$qrc[0]:array());
 	}
 
 }
